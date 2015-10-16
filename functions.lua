94e812644f8737e97fe4fc514b1ea709edc38d1a
Andrea Plati <echoes91@gmail.com>
2015-10-16 20:09:26 +0200

    alpha3

--- functions.lua
+++ functions.lua
@@ -13,3 +13,65 @@ function spears_shot (itemstack, player)
 	obj:get_luaentity().wear = itemstack:get_wear()
 	return true
 end
+
+function spears_set_entity(kind, eq, toughness)
+	local SPEAR_ENTITY={
+		physical = false,
+		timer=0,
+		visual = "wielditem",
+		visual_size = {x=0.15, y=0.1},
+		textures = {"spears:spear_" .. kind},
+		lastpos={},
+		collisionbox = {0,0,0,0,0,0},
+		on_punch = function(self, puncher)
+			if puncher then
+				if puncher:is_player() then
+					local stack = {name='spears:spear_' .. kind, wear=self.wear+65535/toughness}
+					local inv = puncher:get_inventory()
+					if inv:room_for_item("main", stack) then
+						inv:add_item("main", stack)
+						self.object:remove()
+					end
+				end
+			end
+		end,
+	}
+	
+	SPEAR_ENTITY.on_step = function(self, dtime)
+		self.timer=self.timer+dtime
+		local pos = self.object:getpos()
+		local node = minetest.get_node(pos)
+		if not self.wear then
+			self.object:remove()
+			return
+		end
+		if self.lastpos.x~=nil then
+			if node.name ~= "air" and not (string.find(node.name, 'grass') and not string.find(node.name, 'dirt')) and not string.find(node.name, 'flowers:') and not string.find(node.name, 'farming:') then
+				self.object:remove()
+				if self.wear+65535/toughness < 65535 then
+					minetest.add_item(self.lastpos, {name='spears:spear_' .. kind, wear=self.wear+65535/toughness})
+				end
+			elseif self.timer>0.2 then
+				local objs = minetest.get_objects_inside_radius({x=pos.x,y=pos.y,z=pos.z}, 1)
+				for k, obj in pairs(objs) do
+					if obj:get_luaentity() ~= nil then
+						if obj:get_luaentity().name ~= "spears:spear_" .. kind .. "_entity" and obj:get_luaentity().name ~= "__builtin:item" then
+							local speed = vector.length(self.object:getvelocity())
+							local damage = (speed + eq)^1.12-20
+							obj:punch(self.object, 1.0, {
+								full_punch_interval=1.0,
+								damage_groups={fleshy=damage},
+							}, nil)
+							self.object:remove()
+							if self.wear+65535/toughness < 65535 then
+								minetest.add_item(self.lastpos, {name='spears:spear_' .. kind, wear=self.wear+65535/toughness})
+							end
+						end
+					end
+				end
+			end
+		end
+		self.lastpos={x=pos.x, y=pos.y, z=pos.z}
+	end
+	return SPEAR_ENTITY
+end
--- tools.lua
+++ tools.lua
@@ -1,4 +1,4 @@
-function spears_register_spear(kind, desc, eq, toughness, craft)
+function spears_register_spear(kind, desc, eq, toughness, material)
 
 	minetest.register_tool("spears:spear_" .. kind, {
 		description = desc .. " spear",
@@ -28,75 +28,21 @@ function spears_register_spear(kind, desc, eq, toughness, craft)
 		}
 	})
 	
-	local SPEAR_ENTITY={
-		physical = false,
-		timer=0,
-		visual = "wielditem",
-		visual_size = {x=0.15, y=0.1},
-		textures = {"spears:spear_" .. kind},
-		lastpos={},
-		collisionbox = {0,0,0,0,0,0},
-		on_punch = function(self, puncher)
-			if puncher then
-				if puncher:is_player() then
-					local stack = {name='spears:spear_' .. kind, wear=self.wear+65535/toughness}
-					local inv = puncher:get_inventory()
-					if inv:room_for_item("main", stack) then
-						inv:add_item("main", stack)
-						self.object:remove()
-					end
-				end
-			end
-		end,
-	}
-	
-	SPEAR_ENTITY.on_step = function(self, dtime)
-		self.timer=self.timer+dtime
-		local pos = self.object:getpos()
-		local node = minetest.get_node(pos)
-		if not self.wear then
-			self.object:remove()
-			return
-		end
-		
-		if self.lastpos.x~=nil then
-			if node.name ~= "air" and not (string.find(node.name, 'grass') and not string.find(node.name, 'dirt')) and not string.find(node.name, 'flowers:') and not string.find(node.name, 'farming:') then
-				self.object:remove()
-				minetest.add_item(self.lastpos, {name='spears:spear_' .. kind, wear=self.wear+65535/toughness})
-			elseif self.timer>0.2 then
-				local objs = minetest.get_objects_inside_radius({x=pos.x,y=pos.y,z=pos.z}, 1)
-				for k, obj in pairs(objs) do
-					if obj:get_luaentity() ~= nil then
-						if obj:get_luaentity().name ~= "spears:spear_" .. kind .. "_entity" and obj:get_luaentity().name ~= "__builtin:item" then
-							local speed = vector.length(self.object:getvelocity())
-							local damage = (speed + eq)^1.12-20
-							obj:punch(self.object, 1.0, {
-								full_punch_interval=1.0,
-								damage_groups={fleshy=damage},
-							}, nil)
-							self.object:remove()
-							minetest.add_item(self.lastpos, {name='spears:spear_' .. kind, wear=self.wear+65535/toughness})
-						end
-					end
-				end
-			end
-		end
-		self.lastpos={x=pos.x, y=pos.y, z=pos.z}
-	end
+	SPEAR_ENTITY=spears_set_entity(kind, eq, toughness)
 	
 	minetest.register_entity("spears:spear_" .. kind .. "_entity", SPEAR_ENTITY)
 	
 	minetest.register_craft({
-		output = 'spears:spear_' .. kind .. ' 4',
+		output = 'spears:spear_' .. kind,
 		recipe = {
-			{'group:wood', 'group:wood', craft},
+			{'group:wood', 'group:wood', material},
 		}
 	})
 	
 	minetest.register_craft({
-		output = 'spears:spear_' .. kind .. ' 4',
+		output = 'spears:spear_' .. kind,
 		recipe = {
-			{craft, 'group:wood', 'group:wood'},
+			{material, 'group:wood', 'group:wood'},
 		}
 	})
 end
